97bef6549e2d121b2bc43ec81ec5ebfa356371a1
WillowSunny <531062903@qq.com>
2021-05-01 11:13:37 +0800

    3.（必做）改造自定义 RPC 的程序，提交到 GitHub：
    尝试将服务端写死查找接口实现类变成泛型和反射；

---
+++ Week_09/rpc01/rpcfx-core/src/main/java/io/kimmking/rpcfx/server/RpcfxInvoker.java
@@ -0,0 +1,53 @@
+package io.kimmking.rpcfx.server;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import io.kimmking.rpcfx.api.RpcfxRequest;
+import io.kimmking.rpcfx.api.RpcfxResolver;
+import io.kimmking.rpcfx.api.RpcfxResponse;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class RpcfxInvoker {
+
+    private RpcfxResolver resolver;
+
+    public RpcfxInvoker(RpcfxResolver resolver){
+        this.resolver = resolver;
+    }
+
+    public RpcfxResponse invoke(RpcfxRequest request) {
+        RpcfxResponse response = new RpcfxResponse();
+        Class serviceClass = request.getServiceClass();
+
+        // 作业1：改成泛型和反射   这里其实很奇怪，这是使用了泛型而已，但是获取实现类的方式依然是用spring托管的方式
+        // 之前尝试使用反射的方式，但是怎么都无法通过接口类来获取到实现类。
+        Object service = resolver.resolve(serviceClass);//this.applicationContext.getBean(serviceClass);
+
+        try {
+            Method method = resolveMethodFromClass(service.getClass(), request.getMethod());
+            Object result = method.invoke(service, request.getParams()); // dubbo, fastjson,
+            // 两次json序列化能否合并成一个
+            response.setResult(JSON.toJSONString(result, SerializerFeature.WriteClassName));
+            response.setStatus(true);
+            return response;
+        } catch ( IllegalAccessException | InvocationTargetException e) {
+
+            // 3.Xstream
+
+            // 2.封装一个统一的RpcfxException
+            // 客户端也需要判断异常
+            e.printStackTrace();
+            response.setException(e);
+            response.setStatus(false);
+            return response;
+        }
+    }
+
+    private Method resolveMethodFromClass(Class<?> klass, String methodName) {
+        return Arrays.stream(klass.getMethods()).filter(m -> methodName.equals(m.getName())).findFirst().get();
+    }
+
+}
